3ab14371b22ec3d9517118f6926155d80452b6ed
Anton <161358803+Varlenton@users.noreply.github.com>
2024-04-04 21:38:48 +0500

    HomeWorke12

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-import Function1
+def test_function():
+    def inner_function():
+        print('Я в области видимости функции test_function')
+
+    inner_function()
+
+
+test_function()
+
+inner_function()
